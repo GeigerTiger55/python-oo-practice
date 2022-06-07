6100bfb656fc02381c7d04b735192b4a89b87a9a
GeigerTiger55 <elise.geiger@gmail.com>
2022-06-07 15:17:47 -0700

    implemented serial.py

--- serial.py
+++ serial.py
@@ -17,3 +17,22 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self, start):
+        """Create serial number generator starting number from "start"."""
+        self.start = start
+        self.last_serial = start-1
+
+    def __repr__(self):
+        return f"<SerialGenerator start={self.start} last_serial={self.last_serial}>"
+
+    def generate(self):
+        """Increases last_serial and returns last_serial """
+        self.last_serial += 1
+        return self.last_serial
+
+    def reset(self):
+        """Sets last_serial to one less than start number """
+        self.last_serial = self.start-1
+
+
+        
